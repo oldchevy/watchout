90b7a2c442d8646dae7cba30f30137f812efd061
oldchevy <nick.olszowy@gmail.com>
2016-08-06 16:27:38 -0700

    Clean up and comment

--- client/watchout.js
+++ client/watchout.js
@@ -1,18 +1,16 @@
 // start slingin' some d3 here.
 (function() {
 
+  //Game options
   var gameOptions = {
     height: 900,
     width: 1200,
     enemyCount: 30,
-    score: 0
+    score: 0,
+    speed: 2000
   };
 
-  var gameBoard = d3.select('.board')
-                    .append('svg:svg')
-                    .attr('width', gameOptions.width)
-                    .attr('height', gameOptions.height);
-
+  //Generate random data
   var createEnemies = function() {
     return _.range(0, gameOptions.enemyCount).map(function(i) {
       return {
@@ -25,6 +23,7 @@
     });
   };
 
+  //Get a new batch of random data
   var updateEnemies = function(arr) {
     arr.forEach(function(enemy) {
       enemy.x = Math.random() * gameOptions.width;
@@ -32,30 +31,26 @@
     });
   };
 
-  var drag = d3.drag()
-    // .origin(function(d) { return d; })
-    .on('start', dragstarted)
-    .on('drag', dragged)
-    .on('end', dragend);
-
+  //Toggle drag
   var dragstarted = function(d) {
     d3.event.sourceEvent.stopPropagation();
     d3.select(this).classed('dragging', true);
   }; 
-
+  
+  //Change the players position
   var dragged = function(d) {
     d3.select(this)
       .attr('cx', d3.event.x)
       .attr('cy', d3.event.y);
   }; 
 
+  //Un-toggle drag
   var dragend = function(d) {
     d3.event.sourceEvent.stopPropagation();
     d3.select(this).classed('dragging', false);
   }; 
 
-
-
+  //Update helper appends new data and transitions update data
   var update = function(data) {
 
     //Generate new locations
@@ -80,7 +75,7 @@
 
 
     //Update the things currently on the board
-    update.transition().duration(1000)
+    update.transition().duration(gameOptions.speed)
           .attr('x', function(d) { return d.x; })
           .attr('y', function(d) { return d.y; });
 
@@ -88,12 +83,65 @@
     update.exit().remove();
        
   };
+
+
+
+  //Simple collsion counter iterator, feed it to throttle in order to slow down
+  //collision count
+  var collisionCounter = function() {
+    var collisionCount = d3.select('.scoreboard .collisions span');
+    collisionCount.text(+collisionCount.text() + 1);
+  };
+  var slowCollisionCounter = _.throttle(collisionCounter, gameOptions.speed);
+
+
   
+  //Checks for collisions  
+  var checkOverlap = function() {
+
+    //Getting relevant objects for position calculation
+    var enemies = d3.select('body .board svg').selectAll('image.enemy')._groups[0];
+    var player = d3.select('body .board svg').select('circle.mouse')._groups[0][0];
+    var score = d3.select('.scoreboard .current span');
+    var highScore = d3.select('.scoreboard .highscore span');
+    var bool = false;
+
+    //Check if any enemy is nearby
+    enemies.forEach(function(oneEnemy) {
+      if (Math.abs(oneEnemy.x.animVal.value - player.cx.animVal.value) < 10 
+          && Math.abs(oneEnemy.y.animVal.value - player.cy.animVal.value) < 10) {
+        bool = true;
+      }
+    });
+
+    //Changing scores logic, using the throttled collision counter
+    if (bool) {
+      if (+score.text() > +highScore.text()) {
+        highScore.text(+score.text());
+      }
+      score.text(0);
+      gameOptions.score = 0;
+      slowCollisionCounter();
+    } else {
+      gameOptions.score++;
+      score.text(Math.floor(gameOptions.score / 10));
+    }
+  };
+
+
+  //Initialize the game board
+  var gameBoard = d3.select('.board')
+                    .append('svg:svg')
+                    .attr('width', gameOptions.width)
+                    .attr('height', gameOptions.height);
+
+
   //Initialize and update the board
   var myEnemies = createEnemies();
   update(myEnemies);
-  setInterval(function() { update(myEnemies); }, 1000);
+  setInterval(function() { update(myEnemies); }, gameOptions.speed);
 
+  //Initialize player
   d3.select('body .board svg')
     .append('circle')
     .attr('class', 'mouse')
@@ -102,9 +150,16 @@
     .attr('cx', gameOptions.width / 2)
     .attr('cy', gameOptions.height / 2);
 
+  //Initialize event listener for player dragging
   d3.select('body .board svg .mouse').call(d3.drag().on('start', dragstarted)
                                                     .on('drag', dragged)
                                                     .on('end', dragend));
+
+
+  d3.select('body .board')
+    .on('mouseover', function() {
+      setInterval(checkOverlap, 50);
+    });
 
   // Below is event listener for if player icon should ALWAYS follow mouse,
   //instead of just on a drag
@@ -119,49 +174,6 @@
   //       .attr('cy', mouseCoordinates[1]);
 
   //   });
-
-  var collisionCounter = function() {
-    var collisionCount = d3.select('.scoreboard .collisions span');
-    collisionCount.text(+collisionCount.text() + 1);
-  };
-  var slowCollisionCounter = _.throttle(collisionCounter, 1000);
-
-  var checkOverlap = function() {
-
-
-    var enemies = d3.select('body .board svg').selectAll('image.enemy')._groups[0];
-    var player = d3.select('body .board svg').select('circle.mouse')._groups[0][0];
-    var score = d3.select('.scoreboard .current span');
-    var highScore = d3.select('.scoreboard .highscore span');
-    var bool = false;
-
-    enemies.forEach(function(oneEnemy) {
-      if (Math.abs(oneEnemy.x.animVal.value - player.cx.animVal.value) < 10 
-          && Math.abs(oneEnemy.y.animVal.value - player.cy.animVal.value) < 10) {
-        bool = true;
-      }
-    });
-
-    if (bool) {
-      if (+score.text() > +highScore.text()) {
-        highScore.text(+score.text());
-      }
-      score.text(0);
-      gameOptions.score = 0;
-      slowCollisionCounter();
-    } else {
-      gameOptions.score++;
-      score.text(Math.floor(gameOptions.score / 10));
-    }
-
-  
-  };
-
-  d3.select('body .board')
-    .on('mouseover', function() {
-      setInterval(checkOverlap, 50);
-    });
-
 
 
 })();
